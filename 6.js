ca18b24098520434736b97c08e1f4d5d730ab42d
Diana <dianushka.di.jesse@gmail.com>
2023-10-10 14:06:29 +0300

    done 5-8 tasks

--- 6.js
+++ 6.js
@@ -2,43 +2,73 @@
 
 // Клас Basket представляє кошик для покупок з певною стратегією знижки
 class Basket {
-  // Створимо конструктор приймає, що стратегію знижки discountPlan як параметр
-  // Властивість discountPlan отримує значення стратегії знижки, яке було передано конструктору
-  // Створюємо новий пустий масив для зберігання товарів (goods) в кошику
-  // Робимо метод addGood, що приймає один параметр - good, який потрібно додати до масиву
-  // Додаємо новий товар в масив товарів
-  // Робимо метод calculateTotalPrice, що розраховує загальну вартість товарів в кошику з урахуванням знижки
-  // За допомогою метода reduce ми сумуємо вартість всіх товарів в масиві
-  // Застосовуємо знижку до загальної вартості за допомогою метода applyDiscount нашого об'єкта discountPlan
+	// Створимо конструктор приймає, що стратегію знижки discountPlan як параметр
+	// Властивість discountPlan отримує значення стратегії знижки, яке було передано конструктору
+	// Створюємо новий пустий масив для зберігання товарів (goods) в кошику
+	// Робимо метод addGood, що приймає один параметр - good, який потрібно додати до масиву
+	// Додаємо новий товар в масив товарів
+
+	// Робимо метод calculateTotalPrice, що розраховує загальну вартість товарів в кошику з урахуванням знижки
+	// За допомогою метода reduce ми сумуємо вартість всіх товарів в масиві
+	// Застосовуємо знижку до загальної вартості за допомогою метода applyDiscount нашого об'єкта discountPlan
+	constructor(discountPlan) {
+		this.discountPlan = discountPlan;
+	}
+	goods = [];
+	addGood(item) {
+		this.goods.push(item);
+	}
+	calculateTotalPrice() {
+		const totalPrice = this.goods.reduce(
+			(accumulator, product) => accumulator + product.price,
+			0
+		);
+		const discountedPrice = this.discountPlan.applyDiscount(totalPrice);
+		// console.log(discountedPrice);
+		return discountedPrice;
+	}
 }
 
 // Клас RegularDiscountPlan: стратегія знижки для постійних клієнтів
 class RegularDiscountPlan extends Basket {
-  // Робимо метод applyDiscount, що приймає ціну price як параметр
-  // Повертає ціну з урахуванням знижки в 10% price * 0.9
+	// Робимо метод applyDiscount, що приймає ціну price як параметр
+	// Повертає ціну з урахуванням знижки в 10% price * 0.9
+	applyDiscount(price) {
+		return price * 0.9;
+	}
 }
 
 //Клас VIPDiscountPlan: стратегія знижки для VIP клієнтів
 class VIPDiscountPlan extends Basket {
-  // Робимо метод applyDiscount, що приймає ціну price як параметр
-  // Повертає ціну з урахуванням знижки в 20% price * 0.8
+	// Робимо метод applyDiscount, що приймає ціну price як параметр
+	// Повертає ціну з урахуванням знижки в 20% price * 0.8
+	applyDiscount(price) {
+		// console.log(price * 0.8);
+
+		return price * 0.8;
+	}
 }
 
 // Клас NewClientDiscountPlan: стратегія знижки для нових клієнтів
 class NewClientDiscountPlan extends Basket {
-  // Робимо метод applyDiscount, що приймає ціну price як параметр
-  // Повертає ціну з урахуванням знижки в 5% price * 0.95
+	// Робимо метод applyDiscount, що приймає ціну price як параметр
+	// Повертає ціну з урахуванням знижки в 5% price * 0.95
+	applyDiscount(price) {
+		// console.log(price * 0.95);
+
+		return price * 0.95;
+	}
 }
 
 console.log("Завдання 6 ====================================");
 // Після виконання розкоментуйте код нижче
 
 // Створення нового екземпляру кошика зі стратегією знижки для нових клієнтів
-// const basket1 = new Basket(new NewClientDiscountPlan());
+const basket1 = new Basket(new NewClientDiscountPlan());
 
 // Додавання товарів до кошика
-// basket1.addGood({ name: "Product 1", price: 100 });
-// basket1.addGood({ name: "Product 2", price: 50 });
+basket1.addGood({ name: "Product 1", price: 100 });
+basket1.addGood({ name: "Product 2", price: 50 });
 
 // Розрахунок і виведення загальної вартості товарів з урахуванням знижки
-// console.log(basket1.calculateTotalPrice());
+console.log(basket1.calculateTotalPrice());
